013ed4efc77504c45e827a5c0db95510208a1dc3
ramazantufekci <ramazan.tufekci@outlook.com>
2020-11-18 06:41:22 +0300

    step-2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+	var arr = [];
+  res.send(arr)
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
